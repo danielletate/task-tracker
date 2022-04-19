55c672b832404ccd83429d24fee3f8427dd0354d
Danielle Tate <danielle.tate825@gmail.com>
2022-04-19 11:23:03 -0600

    Add components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+   <HeaderItem title="Task Tracker" />
+   <TasksItem :tasks="tasks"/>
+</div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderItem from './components/HeaderItem'
+import TasksItem from './components/TasksItem'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderItem,
+    TasksItem
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  // lifecycle method
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'April 2nd at 2:00pm',
+        reminder: false,
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'May 18th at 2:00pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Interview',
+        day: 'May 21st 2nd at 10:30am',
+        reminder: true,
+      }
+    ]
   }
 }
 </script>
 
+// Global styles
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid rgb(98, 15, 192);
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <ButtonItem text="Add Task" color="#66CD00" />
+    </header>
+</template>
+
+<script>
+import ButtonItem from "./ButtonItem"
+
+export default {
+    name: "HeaderItem",
+    props: {
+        title: String,
+    },
+    components: { ButtonItem }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 20px;
+    color: #3b3b3b;
+}
+</style>
